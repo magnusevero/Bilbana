e4e8b788dc4f7e069b31536e7b7690c778718b37
magnus.evero <magnus.evero@gmail.com>
2013-08-05 22:27:54 +0200

    vymodell basklass förbättrad

--- Bilbana/Bilbana/BaseClasses/ViewModelBase.cs
+++ Bilbana/Bilbana/BaseClasses/ViewModelBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Bilbana.Annotations;
 
@@ -8,14 +9,33 @@ namespace Bilbana.BaseClasses
 	public abstract class ViewModelBase:INotifyPropertyChanged, IDisposable
 	{
 		public string  DisplayName { get; set; }
-
 		public event PropertyChangedEventHandler PropertyChanged;
+		private bool ThrowOnInvalidPropertyName { get; set; }
 
-		[NotifyPropertyChangedInvocator]
-		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		protected virtual void OnPropertyChanged(string propertyName)
 		{
+			VerifyPropertyName(propertyName); 
 			var handler = PropertyChanged;
-			if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+			if (handler == null) 
+				return;
+
+			var e = new PropertyChangedEventArgs(propertyName); 
+			handler(this, e);
+		} 
+		
+		[Conditional("DEBUG")] 
+		[DebuggerStepThrough]
+		private void VerifyPropertyName(string propertyName) 
+		{ 
+			// Verify that the property name matches a real, 
+			// public, instance property on this object. 
+			if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+			{
+				var msg = "Invalid property name: " + propertyName; 
+				if (ThrowOnInvalidPropertyName) 
+					throw new Exception(msg);
+				Debug.Fail(msg);
+			} 
 		}
 
 		public void Dispose()
